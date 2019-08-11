c8dc90ffde3aa7537ecfff43ac109cd7ab3fa00c
Sandro81 <sandro.fioravanti81@gmail.com>
2019-08-11 21:16:19 +0100

    section 3

--- src/app/recipe/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {RecipeModel} from "./recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  // I'll use the RecipeModel as an array for store the information for all components
+  recipes: RecipeModel[] = [];
 
   constructor() { }
 
