adfc3137322bbb88c385a4c1327bec71992310d7
Sandro81 <sandro.fioravanti81@gmail.com>
2019-08-11 21:33:57 +0100

    section 3

--- src/app/recipe/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,9 @@ import {RecipeModel} from "./recipe.model";
 export class RecipeListComponent implements OnInit {
 
   // I'll use the RecipeModel as an array for store the information for all components
-  recipes: RecipeModel[] = [];
+  recipes: RecipeModel[] = [
+    new RecipeModel('Carbonare', 'Carbonara is made with eggs!', 'https://assets.bonappetit.com/photos/5a6f48f94f860a026c60fd71/3:2/w_1028,c_limit/pasta-carbonara.jpg')
+  ];
 
   constructor() { }
 
