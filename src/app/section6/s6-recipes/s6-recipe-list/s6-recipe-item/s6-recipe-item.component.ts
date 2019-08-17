2303545a7cd1d0dc2de84e2f4fae88af8475c3c7
Sandro81 <sandro.fioravanti81@gmail.com>
2019-08-17 19:24:12 +0100

    lesson 77

--- src/app/section6/s6-recipes/s6-recipe-list/s6-recipe-item/s6-recipe-item.component.ts
+++ src/app/section6/s6-recipes/s6-recipe-list/s6-recipe-item/s6-recipe-item.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {RecipeModel} from "../recipe.model";
 
 @Component({
@@ -10,9 +10,21 @@ export class S6RecipeItemComponent implements OnInit {
 
 
   @Input() recipe: RecipeModel;
+  @Input() index: number;
+
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  // We have to attach something to this property to make it listenable from the parent component, @Output decorator.
+  @Output() featureSelected = new EventEmitter<number>();
+
+  onSelect(feature: number){
+    // I want to use this property which now holds this event emitter as a value to emit an event whenever we click one of the buttons and I will then emit feature,
+    this.featureSelected.emit(feature);
+  }
+
 
 }
