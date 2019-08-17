f08e0cebf9c2decd9b64609e34379bc48926f9c1
Sandro81 <sandro.fioravanti81@gmail.com>
2019-08-17 18:45:17 +0100

    lesson 77

--- src/app/section6/s6-recipes/s6-recipe-list/s6-recipe-item/s6-recipe-item.component.ts
+++ src/app/section6/s6-recipes/s6-recipe-list/s6-recipe-item/s6-recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-s6-recipe-item',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class S6RecipeItemComponent implements OnInit {
 
+
+  @Input() recipe: { name: string, desc: string, imagePath: string};
   constructor() { }
 
   ngOnInit() {
