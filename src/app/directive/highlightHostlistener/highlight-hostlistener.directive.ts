9d1c7ca6b3d49f5678e68a78b5bcff65c3bb2142
Sandro81 <sandro.fioravanti81@gmail.com>
2019-08-18 15:26:22 +0100

    lesson 77

--- src/app/directive/highlightHostlistener/highlight-hostlistener.directive.ts
+++ src/app/directive/highlightHostlistener/highlight-hostlistener.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appHighlightHostlistener]'
 })
-export class HighlightHostlistenerDirective {
+export class HighlightHostlistenerDirective implements OnInit{
 
-  constructor() { }
 
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit(): void {
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red');
+  }
+
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'red');
+  }
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'yellow');
+  }
 }
