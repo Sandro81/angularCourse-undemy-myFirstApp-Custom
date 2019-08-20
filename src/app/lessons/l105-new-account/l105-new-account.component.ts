8cad262a68f89b7fab13edd4764a663ef4bb340b
Sandro81 <sandro.fioravanti81@gmail.com>
2019-08-20 21:46:26 +0100

    lesson 77

--- src/app/lessons/l105-new-account/l105-new-account.component.ts
+++ src/app/lessons/l105-new-account/l105-new-account.component.ts
@@ -10,13 +10,15 @@ export class L105NewAccountComponent  {
 
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
+  constructor(private loggingService: LoggingService) {}
+
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
 
-    const service = new LoggingService();
-    service.logStatusChange(accountStatus);
+   /* const service = new LoggingService();
+    service.logStatusChange(accountStatus);*/
   }
 }
