c9469be23a55e4da0816efc8b7aad79f09514000
Sandro81 <sandro.fioravanti81@gmail.com>
2019-08-21 07:00:09 +0100

    lesson 77

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -89,31 +89,5 @@ export class AppComponent {
   //......................
 
 
-  //Section 9
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
-
-  onAccountAdded(newAccount: {name: string, status: string}) {
-    this.accounts.push(newAccount);
-  }
-
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
-  }
-
-
-  //........................
 
 }
